3db1c98b0ce3d45271526aaa8b4b9661825495a5
bang9 <gusrn1423@naver.com>
2026-03-15 21:15:24 +0900

    Revert "feat(shell): add light theme app frame"
    This reverts commit 091a05b2a27bc8c94011a599b93b6d0f48e3cadc.

--- grove/src/App.tsx
+++ grove/src/App.tsx
@@ -1,13 +1,10 @@
 import Layout from "./Layout";
-import { AppShell } from "./components/shell/AppShell";
 import { ToastContainer } from "./components/ui/toast";
 
 function App() {
   return (
     <>
-      <AppShell>
-        <Layout />
-      </AppShell>
+      <Layout />
       <ToastContainer />
     </>
   );
--- grove/src/Layout.tsx
+++ grove/src/Layout.tsx
@@ -5,10 +5,11 @@ import TerminalPanel from "./components/terminal/TerminalPanel";
 import DiffPanel from "./components/diff/DiffPanel";
 import { cn } from "./lib/cn";
 
+
 function Layout() {
   return (
-    <div className={cn("h-full min-h-0 w-full overflow-hidden bg-[var(--color-card)]")}>
-      <Allotment className={cn("h-full w-full bg-[var(--color-card)]")}>
+    <div className={cn("h-full w-full bg-[var(--color-bg)]")}>
+      <Allotment>
         <Allotment.Pane preferredSize={240} minSize={180}>
           <Sidebar />
         </Allotment.Pane>
